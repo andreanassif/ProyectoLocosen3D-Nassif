c464f37cceb8f2cff7483a5b558f21e1943f32ce
Andrea Nassif <andrea.e.nassif@gmail.com>
2022-12-24 11:31:24 -0300

    add ./info + txt .env

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,18 @@
 import * as dotenv from "dotenv";
+import parsedArgs from "minimist";
 
-dotenv.config()
+dotenv.config();
+
+// Llama librería minimist y definir variables defaul
+const options = {default: {puerto:8081, modo: "fork"}}
+
+const objArguments = parsedArgs(process.argv.slice(2), options)
 
 //creamos la configuracion de nuestra aplicacion
+
 export const config = {
+    MODO: objArguments.modo,
+    PORT: objArguments.puerto,
     MARIADB_HOST: process.env.MARIADB_HOST,
     SQLITE_DB: process.env.SQLITE_DB,
     FILE_DB: process.env.FILE_DB,
